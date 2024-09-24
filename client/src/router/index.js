f83d7055dd40149964a25258a80934a52aa0c610
usmanqt <qureshiu@mail.gvsu.edu>
2024-09-24 15:17:51 -0400

    fixed routes error for /, /books and /ping

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Ping from '../components/ping.vue'
 import Books from '../components/Books.vue'
+import HelloWorld from '../components/HelloWorld.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/books',
       name: 'Books',
       component: Books,
     },
